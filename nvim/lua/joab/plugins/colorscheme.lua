93e3f0ad6113146364bca05a58cc6cad50f7a11d
Joab Sena <joabarbosa1@hotmail.com>
2023-10-24 20:38:24 -0300

    nvim(plugin): add gitsigns

--- nvim/lua/joab/plugins/colorscheme.lua
+++ nvim/lua/joab/plugins/colorscheme.lua
@@ -1,20 +1,17 @@
 return {
- -- {
- --    "bluz71/vim-nightfly-guicolors",
- --    priority = 1000, -- make sure to load this before all the other start plugins
- --    config = function()
- --      -- load the colorscheme here
- --      vim.cmd([[colorscheme nightfly]])
- --    end,
- --  },
- {
-    'JoosepAlviste/palenightfall.nvim',
-    priority = 1000,
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme palenightfall]])
-    end,
-    transparent = true
-  },
+	-- {
+	--    "bluz71/vim-nightfly-guicolors",
+	--    priority = 1000, -- make sure to load this before all the other start plugins
+	--    config = function()
+	--      -- load the colorscheme here
+	--      vim.cmd([[colorscheme nightfly]])
+	--    end,
+	--  },
+	{
+		"JoosepAlviste/palenightfall.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme palenightfall]])
+		end,
+	},
 }
-
